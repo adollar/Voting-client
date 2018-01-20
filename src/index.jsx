d08857c99b2367161c660624498ec390852a1156
dev33 <antony.hopkins008@gmail.com>
2018-01-20 22:22:07 +0200

    added tally test Map and put props in Results component

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom'
+import {
+    HashRouter,
+    Route,
+    Switch
+} from 'react-router-dom'
 import {Voting} from "./components/voting";
 import {Results} from "./components/results";
-import {List} from 'immutable';
+import {
+    List,
+    Map
+} from 'immutable';
 import {Navigator} from "./components/navigator";
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
-const routes =
-    <Switch>
-        <Route path="/voting" render={(props) => (
-            <Voting {...props} pair={pair}/>
-        )}/>
-        <Route path="/results" component={Results}/>
-    </Switch>;
+const
+    routes =
+        <Switch>
+            <Route path="/voting" render={(props) => (
+                <Voting {...props} pair={pair} tally={tally}/>
+            )}/>
+            <Route path="/results" render={(props) => (
+                <Results {...props} pair={pair} tally={tally}/>
+            )}/>
+        </Switch>;
 
 ReactDOM.render(
     <HashRouter>
